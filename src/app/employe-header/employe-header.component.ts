e412bb23c5660b77f43cc98918fa2a7f7811bede
Pavle Paunovic <paavlee@yahoo.com>
2023-11-08 11:04:02 +0100

    Little clean-up

--- src/app/employe-header/employe-header.component.ts
+++ src/app/employe-header/employe-header.component.ts
@@ -1,46 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { EmployeHeaderService } from '../employe-header.service';
 import { EmployeFormService } from '../employe-form.service';
 import { EmployeeListService } from '../employee-list-service.service';
 import { EmployeeListHeader } from '../employee-header-list-service.service';
-
-interface Employee {
-  id: number,
-  name: string;
-  address: string
-}
+import { takeUntil } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+import { Employee } from '../models/employee.interface';
 
 @Component({
   selector: 'app-employe-header',
   templateUrl: './employe-header.component.html',
-  styleUrls: ['./employe-header.component.scss']
+  styleUrls: ['./employe-header.component.scss'],
 })
-export class EmployeHeaderComponent {
-  
+export class EmployeHeaderComponent implements OnDestroy {
   employees: Employee[] = [];
   uniqueEmployeeIds = new Set<number>();
-  selectedItem: string = "";
+  selectedItem: string = '';
+  private ngUnsubscribe$ = new Subject<void>();
 
-  constructor(private employeeListService: EmployeeListService, private employeHeaderService: EmployeHeaderService, private employeeListHeader: EmployeeListHeader, private employeeFormService: EmployeFormService) {
-    this.employeeListHeader.employeeList$.subscribe(val => {
-      this.selectedItem = val;
-    });
+  constructor(
+    private employeeListService: EmployeeListService,
+    private employeHeaderService: EmployeHeaderService,
+    private employeeListHeader: EmployeeListHeader,
+    private employeeFormService: EmployeFormService
+  ) {
+    this.employeeListHeader.employeeList$
+      .pipe(takeUntil(this.ngUnsubscribe$))
+      .subscribe((val) => {
+        this.selectedItem = val;
+      });
 
-    this.employeHeaderService.sharedValue$.subscribe((value) => {
-      if (value.id && !this.uniqueEmployeeIds.has(value.id)) {
-        this.employees.push(value);
-        this.uniqueEmployeeIds.add(value.id);
-      }
-    });
-  }
-  showEmployeeForm(employee: Employee) {
-    this.selectedItem = employee.name;
-    this.employeeListService.names$.subscribe((value) => {
-      value.forEach(correctEmployee => {
-        if (employee.id === correctEmployee.id) {
-          this.employeeFormService.showEmployeeForm(correctEmployee);
+    this.employeHeaderService.sharedValue$
+      .pipe(takeUntil(this.ngUnsubscribe$))
+      .subscribe((value) => {
+        if (value.id && !this.uniqueEmployeeIds.has(value.id)) {
+          this.employees.push(value);
+          this.uniqueEmployeeIds.add(value.id);
         }
       });
-    });
-  }  
+  }
+
+  showEmployeeForm(employee: Employee) {
+    this.selectedItem = employee.name;
+    this.employeeListService.names$
+      .pipe(takeUntil(this.ngUnsubscribe$))
+      .subscribe((value) => {
+        value.forEach((correctEmployee) => {
+          if (employee.id === correctEmployee.id) {
+            this.employeeFormService.showEmployeeForm(correctEmployee);
+          }
+        });
+      });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe$.next();
+    this.ngUnsubscribe$.complete();
+  }
 }
--- src/app/main-form/type-of-vacation/type-of-vacation.component.ts
+++ src/app/main-form/type-of-vacation/type-of-vacation.component.ts
@@ -1,40 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { EmployeeListService } from 'src/app/employee-list-service.service';
 import { Employee, VacationType } from 'src/app/models/employee.interface';
 import { EmployeHeaderService } from 'src/app/employe-header.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-type-of-vacation',
   templateUrl: './type-of-vacation.component.html',
   styleUrls: ['./type-of-vacation.component.scss']
 })
-export class TypeOfVacationComponent {
+export class TypeOfVacationComponent implements OnDestroy {
   newVacationType: VacationType = { type: 'New Type', items: [] };
   selectedEmployee: Employee | null = null;
   list: { type: string; items: Employee[] }[] = [];
+  private ngUnsubscribe$ = new Subject<void>();
 
-  constructor(private employeeListService: EmployeeListService, private employeHeaderService: EmployeHeaderService) {
-    this.employeHeaderService.sharedValue$.subscribe((val) => {
-      if (val) {
-        if (!this.selectedEmployee) {
-          this.selectedEmployee = val;
-        } 
-        const selectedEmployee = this.findEmployeeByName(val);
-        if (selectedEmployee) {
-          this.selectEmployee(selectedEmployee);
+  constructor(
+    private employeeListService: EmployeeListService,
+    private employeHeaderService: EmployeHeaderService,
+  ) {
+
+    this.employeHeaderService.sharedValue$
+      .pipe(takeUntil(this.ngUnsubscribe$))
+      .subscribe((val) => {
+        if (val) {
+          if (!this.selectedEmployee) {
+            this.selectedEmployee = val;
+          }
+          const selectedEmployee = this.findEmployeeByName(val);
+          if (selectedEmployee) {
+            this.selectEmployee(selectedEmployee);
+          }
         }
-      } 
-    });
+      });
 
     this.addToList();
   }
-
   addToList() {
-    this.employeeListService.names$.subscribe((val: Employee[]) => {
-      if (val) {
-        this.list = this.transformToExpectedStructure(val);
-      }
-    });
+    this.employeeListService.names$
+      .pipe(takeUntil(this.ngUnsubscribe$))
+      .subscribe((val: Employee[]) => {
+        if (val) {
+          this.list = this.transformToExpectedStructure(val);
+          if (val.length === 1) {
+            this.selectEmployee(val[0]);
+          }
+        }
+      });
   }
 
   private transformToExpectedStructure(employees: Employee[]): { type: string; items: Employee[] }[] {
@@ -49,7 +62,7 @@ export class TypeOfVacationComponent {
         }
         const newVacationType = { type: this.newVacationType.type, items: [] };
         this.selectedEmployee.typeOfVacation.push(newVacationType);
-      }
+      } 
     }
 
   selectEmployee(employee: Employee) {
@@ -65,5 +78,9 @@ export class TypeOfVacationComponent {
       }
     }
     return null;
+  }
+  ngOnDestroy() {
+    this.ngUnsubscribe$.next();
+    this.ngUnsubscribe$.complete();
   }
 }
